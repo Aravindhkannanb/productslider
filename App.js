0258c2fc572e5cac53457af6b22bdc9c66fead34
Aravindhkannanb <99198670+Aravindhkannanb@users.noreply.github.com>
2022-09-25 13:50:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Card from './components/card.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ product.js
@@ -0,0 +1,34 @@
+import img from "./img1.jpeg";
+import img2 from './constructionChemicals.jpg';
+import img3 from './img3.jpg'
+
+export const products=[
+    {
+        "id":1,
+        "image":img2,
+        "head":"Construction chemical",
+        "text":"We Support Some of the Biggest Training Organizations all over the World. We Serve all Time Zones  "    
+    },
+    {
+        "id":1,
+        "image":img2,
+        "head":"Construction chemical",
+        "text":"We Support Some of the Biggest Training Organizations all over the World. We Serve all Time Zones  "    
+    },
+    {
+        "id":2,
+        "image":img,
+        "head":"Paints",
+        "text":"We Support Some of the Biggest Training Organizations all over the World. We Serve all Time Zones "
+  
+    
+    },
+    {
+        "id":3,
+        "image":img3,
+        "head":"kitchen sinks",
+        "text":"We Support Some of the Biggest Training Organizations all over the World. We Serve all Time Zones "
+  
+    
+    }
+  ];
